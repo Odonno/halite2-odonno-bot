a41034b496507808f86fc4791e36980750eb017d
Odonno <bottiau.david@laposte.net>
2018-01-01 18:10:32 +0100

    chore: create first published using C# console project wrapper

---
+++ MyBot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string botName = (args.Length > 0) ? args[0] : "Odonno";
+            Halite2.Main.execute(botName);
+        }
+    }
+}
